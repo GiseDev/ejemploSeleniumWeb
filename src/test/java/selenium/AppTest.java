20f5ccd68a74255364104e00af8c60484df94428
GiseLazcano <glazcano@imed.cl>
2022-03-18 18:07:48 -0300

    versionEntregable

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -3,6 +3,8 @@ package selenium;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,21 +24,42 @@ public class AppTest
     public void setUp(){
         System.out.println("Iniciando configuración...");
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        
         driver = new ChromeDriver();
-        driver.get("https://www.amazon.com");
+        driver.get("https://www.google.com");
         driver.manage().window().maximize();
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-        driver.navigate().to("https://www.google.com");
     }
 
     @Test
     public void shouldAnswerWithTrue()
     {
-        System.out.println("Iniciando Pruebas...");
+        System.out.println("Iniciando Pruebas");
+        System.out.println("Buscar en Google");
         WebElement searchbox = driver.findElement(By.name("q"));
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
-        assertEquals("HandBook Devops", driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
+        driver.findElement(By.linkText("Imágenes")).click();
+        System.out.println("Click imagen");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.linkText("Shopping")).click();
+        System.out.println("Shopping");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
+
+    @Test
+    public void shouldAnswerWithTrueAmazon()
+    {
+        System.out.println("Iniciando Pruebas");
+        System.out.println("Buscar en Amazon");
+        driver.navigate().to("https://www.amazon.com");
+        WebElement searchbox = driver.findElement(By.name("field-keywords"));
+        searchbox.sendKeys("The phoenix project");
+        searchbox.submit();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+    
 }
